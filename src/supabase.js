9e1e4404e713fba0e8071fac9268ccd6875e94d1
Sean Matthew V. Valmocina <sean.matthew.valmocina@adamson.edu.ph>
2025-12-13 21:41:38 +0800

    Update Supabase client import and configuration

--- src/supabase.js
+++ src/supabase.js
@@ -1,11 +1,6 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js/dist/module/index.js';
+import { createClient } from '@supabase/supabase-js';
 
-// 1. Go to https://supabase.com and create a new project
-// 2. Go to Project Settings -> API
-// 3. Paste the URL and ANON KEY below
-
-const supabaseUrl = 'https://kodhmgkjknchysmbjnik.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvZGhtZ2tqa25jaHlzbWJqbmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNzM2NzcsImV4cCI6MjA4MDk0OTY3N30.ymzq3AGJN-_2egWCqdJ7eo3f9QfBH0J27hiHuZI2K40';
+const supabaseUrl = 'https://btxfqgttuttvresjpqwo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0eGZxZ3R0dXR0dnJlc2pwcXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2Mjg4MTEsImV4cCI6MjA4MTIwNDgxMX0.xZ_iPW0XGbuiJwyZ3xRxz0cb8m0CBU4ywT3hC0exKU4';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
